b0915a75aaae4a52d05fe0ad25ce89ad3a46707f
seagull.kamome@gmail.com <seagull.kamome@gmail.com>
2014-04-22 00:06:28 +0900

    Update dependencies

--- build-all.sh
+++ build-all.sh
@@ -1,4 +1,4 @@
 cabal --force-reinstall install
-(cd driver-gdbm; cabal --force-reinstall --sandbox-config-file=../cabal.sandbox.config install)
-(cd driver-hashtables; cabal --force-reinstall --sandbox-config-file=../cabal.sandbox.config install)
+(cd driver-gdbm; cabal --force-reinstall --sandbox-config-file=../cabal.sandbox.config install; cabal sdist)
+(cd driver-hashtables; cabal --force-reinstall --sandbox-config-file=../cabal.sandbox.config install; cabal sdist)
 
